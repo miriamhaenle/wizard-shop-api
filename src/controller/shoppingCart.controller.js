c39a58114d067caac7eb842e02e51c9628c210a9
Miriam <miriam.haenle@gmail.com>
2021-03-06 08:20:52 +0100

    Refactored post Cart to check for existing customer cart

--- src/controller/shoppingCart.controller.js
+++ src/controller/shoppingCart.controller.js
@@ -5,31 +5,40 @@ async function getShoppingCart(req, res) {
   const customerId = req.params.customerId;
 
   const allCarts = await ShoppingCart.find();
-  console.log(allCarts[0].customer._id);
-  const customerCart = allCarts.filter(
-    (cart) => cart.customer._id === customerId
-  );
+  const cart = allCarts.filter((cart) => cart.customer._id == customerId);
 
-  res.json(customerCart);
+  res.json(cart);
 }
 
 async function postShoppingCart(req, res) {
   const customerId = req.params.customerId;
   const customer = await findCustomer(customerId);
-
+  const allCarts = await ShoppingCart.find();
   const orderItems = req.body.orderItems;
-
   const orderSum = calculateSum(orderItems);
 
-  const newShoppingCart = new ShoppingCart({
-    customer: customer,
-    orderItems: req.body.orderItems,
-    orderSum: orderSum,
-  });
-  newShoppingCart
-    .save()
-    .then((shoppingCart) => res.json(shoppingCart))
-    .catch((error) => res.json(error));
+  if (findCustomersCart(allCarts, customerId)) {
+    const existingCart = findCustomersCart(allCarts, customerId);
+
+    existingCart.orderItems.push(orderItems);
+    existingCart.orderSum += orderSum;
+
+    ShoppingCart.findByIdAndUpdate(existingCart._id, existingCart, {
+      new: true,
+    })
+      .then((updatedCart) => res.json(updatedCart))
+      .catch((error) => res.json(error));
+  } else {
+    const newShoppingCart = new ShoppingCart({
+      customer: customer,
+      orderItems: req.body.orderItems,
+      orderSum: orderSum,
+    });
+    newShoppingCart
+      .save()
+      .then((shoppingCart) => res.json(shoppingCart))
+      .catch((error) => res.json(error));
+  }
 }
 
 export { getShoppingCart, postShoppingCart };
@@ -43,4 +52,8 @@ function calculateSum(items) {
     (sum, orderItem) => sum + orderItem.price * orderItem.amount,
     0
   );
+}
+
+function findCustomersCart(carts, customerId) {
+  return carts.find((cart) => cart.customer._id == customerId);
 }
